3e2e43bb0bfe0ce8f0ea7c2cc5af704fd0d225a1
CarrotManMatt <matt@carrotmanmatt.com>
2022-12-13 19:58:53 +0000

    added accounts urls to urls.py & removed named package url inclusions

--- core/urls.py
+++ core/urls.py
@@ -3,11 +3,12 @@
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path("admin/", admin.site.urls, name="admin"),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
     path("", include("pulsifi.urls")),
     path("", RedirectView.as_view(pattern_name="pulsifi:home"), name="default")
 ]
