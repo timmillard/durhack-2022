827c567cd768fcb530fdae7c5df8f6ec1054f057
CarrotManMatt <matt@carrotmanmatt.com>
2022-12-21 19:17:12 +0000

    Added Reddit social account login provider & added codebase VERSION setting

--- pulsifi/admin.py
+++ pulsifi/admin.py
@@ -7,4 +7,4 @@
 # from .models import Profile, Pulse
 
 
-# TODO: add admin pages with multiple permission levels
+# TODO: add admin pages with multiple permission levels, hide first & last names from admin, disable user creation in admin (or test if creating sends signal)
