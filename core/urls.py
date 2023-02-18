41889537658481d3e146d9f0a69201b054c41678
CarrotManMatt <matt@carrotmanmatt.com>
2023-02-18 17:25:08 +0000

    Added admin/docs redirect

--- core/urls.py
+++ core/urls.py
@@ -6,9 +6,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import RedirectView
 
+# noinspection SpellCheckingInspection
 urlpatterns = [
-    path('avatar/', include("avatar.urls")),
-    path('admin/doc/', include("django.contrib.admindocs.urls")),
+    path("avatar/", include("avatar.urls")),
+    path("admin/doc/", include("django.contrib.admindocs.urls")),
+    path("admin/docs/", RedirectView.as_view(pattern_name="django-admindocs-docroot"), name="admindocs_redirect"),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),  # NOTE: Check if the given URL matches one for authentication/account management (only for temporary development use: eventually these views will be implemented within the pulsifi app)
     path("", include("pulsifi.urls")),
