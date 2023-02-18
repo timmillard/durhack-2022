b91d972291571440ad84ca7394c405dbda5706c1
CarrotManMatt <matt@carrotmanmatt.com>
2023-02-18 20:43:14 +0000

    Fixed crashing when trying to clean with in-memory user object when not all fields have values

--- pulsifi/forms.py
+++ pulsifi/forms.py
@@ -84,20 +84,23 @@ class Signup_Form(Base_SignupForm):
 
         self.label_suffix = ""
 
-    def clean(self):
+    def clean(self) -> dict[str]:
+        """ Validate inserted form data using temporary in-memory user object. """
         super().clean()
-        try:
-            get_user_model()(
-                username=self.cleaned_data.get("username"),
-                password=self.cleaned_data.get("password1"),
-                email=self.cleaned_data.get("email")
-            ).full_clean()
-        except ValidationError as e:
-            for field_name, errors in e.error_dict.items():
-                if field_name == "__all__":
-                    self.add_error(errors)
-                else:
-                    self.add_error(field_name, errors)
+        if "username" in self.cleaned_data and "password1" in self.cleaned_data and "email" in self.cleaned_data:
+            try:
+                user = get_user_model()(
+                    username=self.cleaned_data["username"],
+                    password=self.cleaned_data["password1"],
+                    email=self.cleaned_data["email"]
+                )
+                user.full_clean()
+            except ValidationError as e:
+                for field_name, errors in e.error_dict.items():
+                    if field_name == "__all__":
+                        self.add_error(errors)
+                    else:
+                        self.add_error(field_name, errors)
 
         return self.cleaned_data
 
