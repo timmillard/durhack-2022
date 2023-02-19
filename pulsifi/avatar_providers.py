c5309131e2453b6f2fd541ff0f01afc0a5cc14df
CarrotManMatt <matt@carrotmanmatt.com>
2023-02-19 10:28:04 +0000

    Added discord avatar provider debugging

--- pulsifi/avatar_providers.py
+++ pulsifi/avatar_providers.py
@@ -1,4 +1,5 @@
 """ Adapters for use in the pulsifi app. """
+import logging
 
 from allauth.socialaccount.models import SocialAccount
 
@@ -8,6 +9,8 @@ from pulsifi.models import User
 class DiscordAvatarProvider:
     @staticmethod
     def get_avatar_url(user: User, width: int, height: int) -> str | None:
+        logging.debug(user, width, height)
+        # logging.debug(1 << ((width * height) - 1).bit_length())
         try:
             discord_SocialAccount: SocialAccount = user.socialaccount_set.get(provider="discord")
         except user.socialaccount_set.DoesNotExist:
