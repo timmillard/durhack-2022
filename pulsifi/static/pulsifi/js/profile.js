8dea0c26d7b22d7aae3b04f7d697baea4278014c
CarrotManMatt <matt@carrotmanmatt.com>
2023-02-19 11:16:16 +0000

    Added pulse POST forms

--- pulsifi/static/pulsifi/js/profile.js
+++ pulsifi/static/pulsifi/js/profile.js
@@ -5,6 +5,7 @@ function openModal(modal_id) {
     document.getElementById(modal_id).style.display = "block"
     document.getElementById(modal_id).classList.add("show")
 }
+
 function closeModal(modal_id) {
     document.getElementById("backdrop").style.display = "none"
     document.getElementById(modal_id).style.display = "none"
@@ -13,5 +14,6 @@ function closeModal(modal_id) {
 
 // Get the modal
 let newpost_modal = document.getElementById("pulse-create");
-newpost_modal.addEventListener("click", () => {openModal("pulse-create-modal")});
-
+newpost_modal.addEventListener("click", () => {
+    openModal("pulse-create-modal")
+});
