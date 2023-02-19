c60fe8ec9700f5f2a4dc429be2f7d2a7884a17bb
CarrotManMatt <matt@carrotmanmatt.com>
2023-02-19 09:53:00 +0000

    Debugging output of socialaccount adapter

--- pulsifi/adapters.py
+++ pulsifi/adapters.py
@@ -1,10 +1,15 @@
 """ Adapters for use in the pulsifi app. """
 
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from allauth.socialaccount.models import SocialAccount
+
+from pulsifi.models import User
 
 
 class Custom_Pulsifi_SocialAccountAdapter(DefaultSocialAccountAdapter):
     # noinspection SpellCheckingInspection
-    def populate_user(self, request, sociallogin, data):
+    def populate_user(self, request, sociallogin: SocialAccount, data: dict[str, ...]) -> User:
         print("here")
+        print(sociallogin)
+        print(sociallogin.get_avatar_url())
         return super().populate_user(request, sociallogin, data)
