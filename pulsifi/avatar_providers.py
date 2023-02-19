87ff091fbcdbab95ffc87b3de5c3c3415960a287
CarrotManMatt <matt@carrotmanmatt.com>
2023-02-19 10:46:28 +0000

    Fixed discord avatar size requires rounding

--- pulsifi/avatar_providers.py
+++ pulsifi/avatar_providers.py
@@ -8,9 +8,15 @@ from pulsifi.models import User
 class DiscordAvatarProvider:
     @staticmethod
     def get_avatar_url(user: User, width: int, height: int) -> str | None:
+        size = 1 << ((width * height) - 1).bit_length()
+        if size > 4096:
+            size = 4096
+        elif size < 16:
+            size = 16
+
         try:
             discord_SocialAccount: SocialAccount = user.socialaccount_set.get(provider="discord")
         except SocialAccount.DoesNotExist:
             return
         else:
-            return f"{discord_SocialAccount.get_avatar_url()}/?size={1 << ((width * height) - 1).bit_length()}"
+            return f"{discord_SocialAccount.get_avatar_url()}/?size={size}"
